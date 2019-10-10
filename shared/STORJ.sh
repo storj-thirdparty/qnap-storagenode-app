7f49671cf84a6162f68d9793d806fecbbf590095
Ninad Thakare <ninad.thakare@neosofttech.com>
2019-10-09 21:18:24 -1100

    First Commit

---
+++ shared/STORJ.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+CONF=/etc/config/qpkg.conf
+QPKG_NAME="STORJ"
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
+APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
+export QNAP_QPKG=$QPKG_NAME
+
+case "$1" in
+  start)
+    ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
+    if [ "$ENABLED" != "TRUE" ]; then
+        echo "$QPKG_NAME is disabled."
+        exit 1
+    fi
+    : ADD START ACTIONS HERE
+    ;;
+
+  stop)
+    : ADD STOP ACTIONS HERE
+    ;;
+
+  restart)
+    $0 stop
+    $0 start
+    ;;
+
+  *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+
+exit 0
