83b60a135572fb0b934482508147e04a883ff46f
sujeet <sujeet.gupta@neosofttech.com>
2019-11-15 16:39:33 +0530

    Fixed iframe design issue on dashboard page

--- shared/web/dashboard.php
+++ shared/web/dashboard.php
@@ -6,15 +6,16 @@
     <div class="row">
       <?php include 'menu.php'; ?>
       <div class="col-10">
-        <div class="container-fluid">
+        <div class="container-fluid dashboard-view">
           <?php
               $ip = "http://173.225.183.161";
               $_finalIp = $ip.":14002/".
               $output = shell_exec("/etc/init.d/STORJ.sh is-running");
               if (!trim($output) == "") { ?>
                <iframe src="<?php echo $_finalIp; ?>" width="100%" height="100%"></iframe>
-             <?php } else {
-               echo $output;
+             <?php } else { ?>
+               <iframe src="<?php echo $_finalIp; ?>" width="100%" height="100%"></iframe>
+               <?php echo $output;
               } ?>
         <div>
       </div>
