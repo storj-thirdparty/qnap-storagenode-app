60f549542ec7aa68051a91962afaeb06f98d92d2
Mrityunjay <kmrityunjay@gmail.com>
2020-02-03 01:43:06 +0530

    Permisisons management

--- shared/web/scripts/storagenodestart.sh
+++ shared/web/scripts/storagenodestart.sh
@@ -6,7 +6,6 @@ echo `date` "Storagenode is starting" >> $LOG
 
 export PATH=$PATH:/share/CACHEDEV1_DATA/.qpkg/container-station/bin
 IPADDR=$(ip -4 -o addr show eth0 | awk '{print $4}' | cut -d "/" -f 1)
-chmod 666 /share/CACHEDEV1_DATA/.qpkg/STORJ/web/config.json
 
 cmd="docker run -d --restart no -p \"${1}\":28967 -p 14002:14002 -e WALLET=\"${2}\" -e EMAIL=\"${3}\" -e ADDRESS=\"${IPADDR}:${1}\" -e BANDWIDTH=\"${4}TB\" -e STORAGE=\"${5}TB\" -v ${6}:/app/identity -v ${7}:/app/config --name storagenode storjlabs/storagenode:beta "
 echo `date` " Starting Storagenode ---> " >> $LOG
