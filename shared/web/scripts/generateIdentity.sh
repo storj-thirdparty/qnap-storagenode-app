3056a1ab3705fb6b1d9967688b4a313d851247df
Mrityunjay <kmrityunjay@gmail.com>
2020-04-29 16:27:16 +0530

    Updated Create identity script

--- shared/web/scripts/generateIdentity.sh
+++ shared/web/scripts/generateIdentity.sh
@@ -15,6 +15,9 @@ function logMessage {
 }
 
 selfName=`basename $0`
+scriptDir=`dirname $0`
+identityFileDir=`dirname $scriptDir`
+
 logMessage "==== Generate Identity called ($@) ============"
 if [[ $# -lt 1 ]] 
 then
@@ -29,9 +32,11 @@ identityBase=/share/Public/identity
 keyBase=${home}/.local/share/storj/identity
 
 identityLogFile=${identityBase}/logs/storj_identity.log
-identityPidFile=${identityBase}/logs/identity.pid
+#identityPidFile=${identityBase}/logs/identity.pid
 identityDirPath=${identityBase}/storagenode
 identityBinary=${identityBase}.bin/identity
+
+identityPidFile=${identityFileDir}/identity.pid
 
 identityKey=${keyBase}/storagenode/identity.key
 caKey=${keyBase}/storagenode/ca.key
@@ -48,13 +53,17 @@ logMessage "Running $identityBinary create storagenode "
 $identityBinary create storagenode --identity-dir ${keyBase}  > ${identityLogFile} 2>&1 & 
 BG_PID=$!
 echo ${BG_PID} > ${identityPidFile}
-logMessage "$identityBinary launched with PID ${BG_PID}. Going to wait for it"
-tail --pid=${BG_PID} -f /dev/null
-logMessage "Identity key generation completed (STEP#1) "
+logMessage "$identityBinary launched with PID ${BG_PID}. Going to wait for it to complete"
+while  [ -d "/proc/${BG_PID}" ]
+do
+    sleep 1
+done
+logMessage "Identity key generation (${identityBinary}:${BG_PID} completed (STEP#1) "
 
 if [[ ! -f $identityKey  ]]
 then
     logMessage "ERROR: Identity key not generated on run" 
+    rm ${identityPidFile}
     exit 3 
 fi
 
@@ -62,17 +71,19 @@ count=$(/bin/ls $identityDirPath | wc -l)
 if [[ $count -lt 4 ]]
 then
     logMessage "ERROR: All Identity files not generated on run" 
+    #rm ${identityPidFile}
     #exit 4
 fi
 
-logMessage "Authorizing identity using identity key string "
+logMessage "Authorizing identity using identity key string (IdentityPidRef:${BG_PID}) "
 logMessage "Running $identityBinary authorize storagenode $identityString --identity-dir /root/.local/share/storj/identity --signer.tls.revocation-dburl bolt:///root/.local/share/storj/identity/revocations.db "
 $identityBinary authorize storagenode $identityString --identity-dir /root/.local/share/storj/identity --signer.tls.revocation-dburl bolt:///root/.local/share/storj/identity/revocations.db
 
 count=$(/bin/ls $identityDirPath | wc -l)
 if [[ $count -lt 6 ]]
 then
-    logMessage "Error: Authorization of Identity Signature has possibly failed (Only $count files found)!!"
+    logMessage "Error: Authorization of Identity Signature has possibly failed (Only $count files found)(IdentityPidRef:${BG_PID})!!"
+    rm ${identityPidFile}
     exit 5
 fi
 
@@ -81,15 +92,18 @@ numBeginId=`grep -c BEGIN /root/.local/share/storj/identity/storagenode/identity
 
 if [[ $numBeginCa -ne 2 ]]
 then
-    logMessage "Error: Authorization has failed (#begin in CA=$numBeginCa) "
+    logMessage "Error: Authorization has failed (#begin in CA=$numBeginCa) (IdentityPidRef:${BG_PID})"
+    rm ${identityPidFile}
     exit 6
 fi
 if [[ $numBeginId -ne 3 ]]
 then
-    logMessage "Error: Authorization has failed (#begin in ID=$numBeginId) "
+    logMessage "Error: Authorization has failed (#begin in ID=$numBeginId) (IdentityPidRef:${BG_PID})"
+    rm ${identityPidFile}
     exit 7
 fi
-logMessage "Authorization of Identity Signature Completed (STEP #2)"
-logMessage "Identity Generation Successfully completed"
+logMessage "Authorization of Identity Signature Completed (STEP #2)(IdentityPidRef:${BG_PID})"
+logMessage "Identity Generation Successfully completed(IdentityPidRef:${BG_PID})"
 logMessage Done
+rm ${identityPidFile}
 exit 0
