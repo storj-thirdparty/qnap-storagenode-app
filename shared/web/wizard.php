8fc6dc44ac412f9cc25e4de23b14366fa13241bb
Mrityunjay <kmrityunjay@gmail.com>
2020-04-30 19:33:16 +0530

    Updated Wizard

--- shared/web/wizard.php
+++ shared/web/wizard.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <head>
 	<link href="resources/css/wizard.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+	<?php
+		$file = "config.json";
+		if(file_exists($file)){
+		  	$content = file_get_contents($file);
+		  	$prop = json_decode($content, true);
+		  }
+	?>
 
 	<div id="app">
+		<a href="config.php"><i class="fa fa-home homeicon"></i></a>
 		<div v-bind:class="stepClass">
 
 			<img class="back" src="resources/img/back.png" v-if="step > 1" v-on:click="step--">
@@ -24,7 +34,7 @@
 				<p class="tagline">In order to recieve and hold your STORJ token payouts, you need an ERC-20 compatible wallet address</p>
 
 				<label class="email-label">Email Address</label>
-				<input type="email" class="email" placeholder="mail@default.com" v-model="email" v-bind:class="{ invalid: !emailValid }">
+				<input type="email" class="email" placeholder="mail@default.com" v-model="email" v-bind:class="{ invalid: !emailValid }" value="<?php if(isset($prop['Email'])) echo $prop['Email'] ?>">
 
 				<button class="skip" v-on:click="step++">Skip this step</button>
 				<button class="continue" v-on:click="step++" v-bind:disabled="!emailValid">Continue</button>
@@ -37,7 +47,7 @@
 				<p class="tagline">In order to recieve and hold your STORJ token payouts, you need an ERC-20 compatible wallet address</p>
 
 				<label class="address-label">ETH Wallet Address</label>
-				<input type="text" class="address" placeholder="Enter ETH Wallet Address" v-model="address" v-bind:class="{ invalid: !addressValid }">
+				<input type="text" class="address" placeholder="Enter ETH Wallet Address" v-model="address" v-bind:class="{ invalid: !addressValid }" value="<?php if(isset($prop['Wallet'])) echo $prop['Wallet'] ?>">
 
 				<button class="continue" v-on:click="step++" v-bind:disabled="!addressValid">Continue</button>
 			</div>
@@ -49,7 +59,7 @@
 				<p class="tagline">How much disk space you want to allocate to the Storj network</p>
 
 				<label class="storage-label">Storage Allocation</label>
-				<input class="storage" type="number" min="1" max="1000" value="1000000" v-model="storage" v-bind:class="{ invalid: !storageValid }">
+				<input class="storage" type="number" min="1" max="1000" value="1000000" v-model="storage" v-bind:class="{ invalid: !storageValid }" value="<?php if(isset($prop['Allocation'])) echo $prop['Allocation'] ?>">
 				<span class="unit">GB</span>
 
 				<button class="continue" v-on:click="step++" v-bind:disabled="!storageValid">Continue</button>
@@ -62,7 +72,7 @@
 				<p class="tagline">The local directory where you want files to be stored on your hard drive for the network</p>
 
 				<label class="directory-label">Storage Directory</label>
-				<input class="directory" type="text" placeholder="/path/to/folder_to_share" v-model="directory" v-bind:class="{ invalid: !directoryValid }">
+				<input class="directory" type="text" placeholder="/path/to/folder_to_share" v-model="directory" v-bind:class="{ invalid: !directoryValid }" value="<?php if(isset($prop['Directory'])) echo $prop['Directory'] ?>">
 
 				<button class="continue" v-on:click="step++" v-bind:disabled="!directoryValid">Continue</button>
 			</div>
@@ -74,7 +84,7 @@
 				<p class="tagline">How a storage node communicates with others on the Storj network, even though it is behind a router. You need a dynamic DNS service to ensure your storage node is connected</p>
 
 				<label class="host-label">Host Address</label>
-				<input class="host" type="text" placeholder="hostname.ddns.net:28967" v-model="host" v-bind:class="{ invalid: !hostValid }">
+				<input class="host" type="text" placeholder="hostname.ddns.net:28967" v-model="host" v-bind:class="{ invalid: !hostValid }" value="<?php if(isset($prop['Port'])) echo $prop['Port'] ?>">
 
 				<button class="continue" v-on:click="step++" v-bind:disabled="!hostValid">Continue</button>
 			</div>
@@ -87,10 +97,10 @@
 					<p class="tagline">Every node is required to have a unique identifier on the network. If you haven't already, get an authorization token. Please get the authorization token and create identity on host machine other than NAS</p>
 
 					<label class="identity-label">Identity Path</label>
-					<input class="identity" type="text" placeholder="/path/to/identity" v-model="identity">
+					<input class="identity" type="text" placeholder="/path/to/identity" v-model="identity" value="<?php if(isset($prop['Identity'])) echo $prop['Identity'] ?>" v-bind:class="{ invalid: !identityValid }">
 
-					<button class="no-identity" v-on:click="identityStep++">I don't have an identity</button>
-					<button class="finish" v-on:click="step++">Finish</button>
+					<button class="no-identity" v-on:click="identityStep++" v-bind:disabled="!identityValid">I don't have an identity</button>
+					<button class="finish" v-on:click="step++" v-bind:disabled="!identityValid">Finish</button>
 				</div>
 
 				<div class="identity-step-2" v-if="identityStep === 2">
@@ -100,7 +110,7 @@
 					<p class="tagline">Every node is required to have a unique identifier on the network. If you haven't already, get an authorization token. Please get the authorization token and create identity on host machine other than NAS</p>
 
 					<label>Authorization Token</label>
-					<input  type="text" placeholder="your@email.com: 1BTJeyYWAquvfQWscG9VndHjyYk8PSzQvrJ5DC" id="authkey" class="invalid"><br><br><br><br>
+					<input  type="text" placeholder="your@email.com: 1BTJeyYWAquvfQWscG9VndHjyYk8PSzQvrJ5DC" id="authkey"  value="<?php if(isset($prop['AuthKey'])) echo $prop['AuthKey'] ?>" :value="authkey"><br><br><br><br>
 
 
 					<button class="generate" v-on:click="generateIdentity">Generate</button>
@@ -131,7 +141,7 @@
 				<button class="finish" v-on:click="finish">Finish</button>
 			</div>
 
-			<div class="progress" v-if="step > 1">
+			<div class="progress" v-if="step > 1" style="-webkit-box-shadow: 0px;box-shadow: inset 0 1px 2px #fff;background-color: white;">
 				<div class="point-1" v-bind:class="{ 'point-active': step > 1 }"></div>
 				<div class="label-1" v-bind:class="{ 'label-active': step > 1 }">1</div>
 				<div class="text-1" v-bind:class="{ 'text-active': step === 2 }">Email Address</div>
@@ -173,5 +183,31 @@
 	<script src="resources/js/axios.min.js"></script>
 	<script src="resources/js/wizard.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+	<div class="container">
+	  <!-- Modal -->
+	  <div class="modal fade" id="myModal" role="dialog">
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Wizard</h4>
+	        </div>
+	        <div class="modal-body">
+	          <p>Identity creating at <b>/root/.local/share/storj/identity/storagenode</b></p>
+	        </div>
+	        <div class="modal-footer">
+	          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        </div>
+	      </div>
+	      
+	    </div>
+	  </div>
+	  
+	</div>
 
 </body>
