4e94efc19319d2caa72f7e6f924d0da437416b75
Mrityunjay <kmrityunjay@gmail.com>
2020-06-10 15:35:03 +0530

    Package routine updates to extract path at the install time

---
+++ shared/web/scripts/resource.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PKGNAME=STORJ
+LOGFILE=/var/log/${PKGNAME}
+IDENTITY_PATH=/share/Public/identity
+export LOGFILE IDENTITY_PATH
+
+# --- Automatically updated by install script . Don't change ----
+
