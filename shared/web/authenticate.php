b28c9b04131349f477b02496263cf58758889459
Parag Padsumbiya <parag.kumar@neosofttech.com>
2020-07-24 19:46:17 +0530

    Changes for the login functionality

--- shared/web/authenticate.php
+++ shared/web/authenticate.php
@@ -6,7 +6,6 @@ $cURLConnection = curl_init();
 
 
 $host = "http://$_SERVER[HTTP_HOST]";
-$host = "http://173.225.183.161:8080";
 
 
 $url = $host . '/cgi-bin/authLogin.cgi?user=' . $data['username'] . '&pwd=' . $data['password'] . '&remme=1';
