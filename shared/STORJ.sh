b6d2cacc63a0b7af41008812114252832983100e
Parag Padsumbiya <parag.kumar@neosofttech.com>
2021-03-31 18:14:46 +0530

    WIP changes in qpkg files for the lumen integration

--- shared/STORJ.sh
+++ shared/STORJ.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONF=/etc/config/qpkg.conf
-QPKG_NAME="STORJ_LUMEN_TEST"
+QPKG_NAME="STORJ"
 LOGFILE=/var/log/${QPKG_NAME}
 QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
 APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
@@ -8,7 +8,7 @@ export QNAP_QPKG=$QPKG_NAME
 
 case "$1" in
   start)
-    echo `date` " STORJ_LUMEN_TEST : Request to start ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request to start ($@) " >> $LOGFILE
     ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
     if [ "$ENABLED" != "TRUE" ]; then
         echo "$QPKG_NAME is disabled."
@@ -16,25 +16,25 @@ case "$1" in
         exit 1
     fi
     : ADD START ACTIONS HERE
-    echo `date` "Create $QPKG_ROOT/web folder link to STORJ folder " >> /tmp/runSTORJ.log
-    ln -s $QPKG_ROOT/web /home/Qhttpd/Web/STORJ_LUMEN_TEST
-    echo `date` " STORJ_LUMEN_TEST : Request to start ($@) completed " >> $LOGFILE
+    echo `date` "Create $QPKG_ROOT/web/public folder link to STORJ folder " >> /tmp/runSTORJ.log
+    ln -s $QPKG_ROOT/web/public /home/Qhttpd/Web/STORJ
+    echo `date` " STORJ : Request to start ($@) completed " >> $LOGFILE
     ;;
 
   stop)
-    echo `date` " STORJ_LUMEN_TEST : Request to stop ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request to stop ($@) " >> $LOGFILE
     : ADD STOP ACTIONS HERE
-    rm /home/Qhttpd/Web/STORJ_LUMEN_TEST
+    rm /home/Qhttpd/Web/STORJ
     ;;
 
   restart)
-    echo `date` " STORJ_LUMEN_TEST : Request restart ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request restart ($@) " >> $LOGFILE
     $0 stop
     $0 start
     ;;
 
   *)
-    echo `date` " STORJ_LUMEN_TEST : Request UNKNOWN ($@) " >> $LOGFILE
+    echo `date` " STORJ : Request UNKNOWN ($@) " >> $LOGFILE
     echo "Usage: $0 {start|stop|restart}"
     exit 1
 esac
