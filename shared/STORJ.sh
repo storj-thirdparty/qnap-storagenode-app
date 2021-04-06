730530b2f0d92e866e45f013f9c95e2c01f562ec
Parag Padsumbiya <parag.kumar@neosofttech.com>
2021-04-06 12:44:49 +0530

    WIP setup for separate app

--- shared/STORJ.sh
+++ shared/STORJ.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONF=/etc/config/qpkg.conf
-QPKG_NAME="STORJ"
+QPKG_NAME="STORJ_LUMEN"
 LOGFILE=/var/log/${QPKG_NAME}
 QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
 APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
@@ -8,33 +8,33 @@ export QNAP_QPKG=$QPKG_NAME
 
 case "$1" in
   start)
-    echo `date` " STORJ : Request to start ($@) " >> $LOGFILE
+    echo `date` " STORJ_LUMEN : Request to start ($@) " >> $LOGFILE
     ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
     if [ "$ENABLED" != "TRUE" ]; then
         echo "$QPKG_NAME is disabled."
-	echo `date` " STORJ : $PKG_NAME is disabled " >> $LOGFILE
+	echo `date` " STORJ_LUMEN : $PKG_NAME is disabled " >> $LOGFILE
         exit 1
     fi
     : ADD START ACTIONS HERE
     echo `date` "Create $QPKG_ROOT/web/public folder link to STORJ folder " >> /tmp/runSTORJ.log
-    ln -s $QPKG_ROOT/web/public /home/Qhttpd/Web/STORJ
-    echo `date` " STORJ : Request to start ($@) completed " >> $LOGFILE
+    ln -s $QPKG_ROOT/web/public /home/Qhttpd/Web/STORJ_LUMEN
+    echo `date` " STORJ_LUMEN : Request to start ($@) completed " >> $LOGFILE
     ;;
 
   stop)
-    echo `date` " STORJ : Request to stop ($@) " >> $LOGFILE
+    echo `date` " STORJ_LUMEN : Request to stop ($@) " >> $LOGFILE
     : ADD STOP ACTIONS HERE
-    rm /home/Qhttpd/Web/STORJ
+    rm /home/Qhttpd/Web/STORJ_LUMEN
     ;;
 
   restart)
-    echo `date` " STORJ : Request restart ($@) " >> $LOGFILE
+    echo `date` " STORJ_LUMEN : Request restart ($@) " >> $LOGFILE
     $0 stop
     $0 start
     ;;
 
   *)
-    echo `date` " STORJ : Request UNKNOWN ($@) " >> $LOGFILE
+    echo `date` " STORJ_LUMEN : Request UNKNOWN ($@) " >> $LOGFILE
     echo "Usage: $0 {start|stop|restart}"
     exit 1
 esac
