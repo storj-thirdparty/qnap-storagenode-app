c782bd38567ae856fa71b2d9fa9413fd0ea3e629
Parag Padsumbiya <parag.kumar@neosofttech.com>
2021-04-29 19:09:30 +0530

    revert changes

--- shared/web/public/index.php
+++ shared/web/public/index.php
@@ -2,8 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-ini_set('session.cookie_httponly', 1);
-
 /*
 |--------------------------------------------------------------------------
 | Create The Application
