d75523323b25f3f21989efc87c18ca98be81e956
Parag Padsumbiya <parag.kumar@neosofttech.com>
2021-04-29 19:07:25 +0530

    set cookie for http only

--- shared/web/public/index.php
+++ shared/web/public/index.php
@@ -2,6 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+ini_set('session.cookie_httponly', 1);
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
